0c8a4f59b153075dfb9cb9d6a612186b4c0d9602
con27 <condronawanto@gmail.com>
2025-01-29 23:28:14 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+mysql_connect("localhost","root","t_siswa");
+ 
+// isikan dengan nama database yang akan di hubungkan
+mysql_select_db("t_siswa");
+?>
